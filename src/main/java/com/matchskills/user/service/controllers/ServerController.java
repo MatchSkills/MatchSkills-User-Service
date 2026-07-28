e7177d5a5fbd44125f5ac24411370849e1c20f9b
Mayck Gomes <mayck.gomes2901@gmail.com>
2026-07-28 15:11:48 -0300

    feat: add h2 database for tests feat: add health for tests

---
+++ src/main/java/com/matchskills/user/service/controllers/ServerController.java
@@ -0,0 +1,18 @@
+package com.matchskills.user.service.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class ServerController {
+
+    @GetMapping
+    public ResponseEntity<Void> health() {
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
+}
